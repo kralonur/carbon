0c258e9a778c3feb5b4e0a986961a571cda0776c
kralonur <kralonur1998@gmail.com>
2025-10-08 20:26:28 +0300

    feat: add swap 2 events types

--- decoders/meteora-damm-v2-decoder/src/instructions/evt_swap2_event.rs
+++ decoders/meteora-damm-v2-decoder/src/instructions/evt_swap2_event.rs
@@ -1,7 +1,22 @@
 use carbon_core::{borsh, CarbonDeserialize};
 
+use crate::types::{SwapParameters2, SwapResult2};
+
 #[derive(
     CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
 )]
 #[carbon(discriminator = "0xe445a52e51cb9a1dbd4233a826507599")]
-pub struct EvtSwap2Event {}
+pub struct EvtSwap2Event {
+    pub pool: solana_pubkey::Pubkey,
+    pub trade_direction: u8,
+    pub collect_fee_mode: u8,
+    pub has_referral: bool,
+    pub params: SwapParameters2,
+    pub swap_result: SwapResult2,
+    pub included_transfer_fee_amount_in: u64,
+    pub included_transfer_fee_amount_out: u64,
+    pub excluded_transfer_fee_amount_out: u64,
+    pub current_timestamp: u64,
+    pub reserve_a_amount: u64,
+    pub reserve_b_amount: u64,
+}
